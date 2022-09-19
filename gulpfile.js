bf011619c81a42524f0e8437bbc1a44f508fd506
luvlore <brunaluchini22@gmail.com>
2022-09-18 22:47:47 -0300

    feat: create header, about, cards and office

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,18 @@
 const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+
+const cssSrc = './src/styles/*.css';
+
+const cssDist = './src/';
+const cssDistName = 'style.css';
+
+gulp.task('styles', () => gulp
+  .src(cssSrc)
+  .pipe(plumber())
+  .pipe(uglify())
+  .pipe(concat(cssDistName))
+  .pipe(gulp.dest(cssDist))
+);
